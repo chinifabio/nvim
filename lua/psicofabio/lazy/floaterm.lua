8d8fd3da3ce50f1d30da3aea16ceb26ab02ed146
chinifabio <fabiochini99@gmail.com>
2024-10-22 10:30:43 +0200

    Update keybinds

--- lua/psicofabio/lazy/floaterm.lua
+++ lua/psicofabio/lazy/floaterm.lua
@@ -3,5 +3,8 @@ return {
     config = function()
         vim.g.floaterm_width = 0.8
         vim.g.floaterm_height = 0.8
+
+        -- local floatterm = require("vim-floaterm")
+        vim.keymap.set("n", "<leader>tt", vim.cmd.FloatermToggle, { desc = "[T]oggle [T]terminal" })
     end,
 }
--- lua/psicofabio/remaps.lua
+++ lua/psicofabio/remaps.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pf", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "open [F]ile [E]xplorer" })
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")     -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")     -- move line down(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
